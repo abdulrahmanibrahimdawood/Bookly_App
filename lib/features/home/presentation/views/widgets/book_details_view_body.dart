687e3d2c9b068af4298b627ffcbdc2c2496c77e3
abdulrahmanibrahimdawood <abdoibrahim9111@gmail.com>
2025-03-21 18:29:15 +0200

    refactor book details view

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,9 +1,6 @@
-import 'package:bookly_app/core/utils/styles.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/book_rating.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/books_action.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/books_details_section.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_item.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/similer_books_list_view.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/similer_books_section.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -11,7 +8,6 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var width = MediaQuery.of(context).size.width;
     return CustomScrollView(
       slivers: [
         SliverFillRemaining(
@@ -21,52 +17,14 @@ class BookDetailsViewBody extends StatelessWidget {
             child: Column(
               children: [
                 CustomBookDetailsAppBar(),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: width * .2),
-                  child: CustomBookImage(),
-                ),
-                SizedBox(height: 43),
-                Text(
-                  'The Jungle Book',
-                  style:
-                      Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),
-                ),
-                SizedBox(height: 6),
-                Opacity(
-                  opacity: .7,
-                  child: Text(
-                    'Rudyard Kipling',
-                    style: Styles.textStyle18.copyWith(
-                      fontWeight: FontWeight.w500,
-                      fontStyle: FontStyle.italic,
-                    ),
-                  ),
-                ),
-                SizedBox(height: 18),
-                BookRating(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                ),
-                SizedBox(
-                  height: 37,
-                ),
-                BooksAction(),
+                BookDetailsSection(),
                 Expanded(
                   child: SizedBox(
                     height: 50,
                   ),
                 ),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'You can also like',
-                    style: Styles.textStyle14.copyWith(
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ),
-                SizedBox(height: 16),
-                SimilerBooksListView(),
-                SizedBox(height: 10),
+                SimilerBooksSection(),
+                SizedBox(height: 15),
               ],
             ),
           ),
---
+++ lib/features/home/presentation/views/widgets/similer_books_section.dart
@@ -0,0 +1,24 @@
+import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/similer_books_list_view.dart';
+import 'package:flutter/material.dart';
+
+class SimilerBooksSection extends StatelessWidget {
+  const SimilerBooksSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'You can also like',
+          style: Styles.textStyle14.copyWith(
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        SizedBox(height: 16),
+        SimilerBooksListView(),
+      ],
+    );
+  }
+}

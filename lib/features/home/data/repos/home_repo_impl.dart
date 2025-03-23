230441197a808c6de3e91b13e7edbd21271aeed0
abdulrahmanibrahimdawood <abdoibrahim9111@gmail.com>
2025-03-23 03:12:31 +0200

    implement fetch featured books

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -34,8 +34,26 @@ class HomeRepoImpl implements HomeRepo {
   }
 
   @override
-  Future<Either<Failures, List<BookModel>>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<Either<Failures, List<BookModel>>> fetchFeaturedBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint: 'volumes?Filtering=free-ebooks&q=subject:Programming');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } on Exception catch (e) {
+      if (e is DioException) {
+        return left(
+          ServerFailures.fromDioError(e),
+        );
+      }
+      return left(
+        ServerFailures(
+          errorMessage: e.toString(),
+        ),
+      );
+    }
   }
 }
